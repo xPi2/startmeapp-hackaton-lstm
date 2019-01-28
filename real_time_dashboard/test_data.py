1cdd672e0deebc8700a807237488a1642956871f
davidm4r <david.copo@gmail.com>
2019-01-28 19:23:30 +0100

    Better accumulative

--- real_time_dashboard/test_data.py
+++ real_time_dashboard/test_data.py
@@ -5,32 +5,32 @@ import requests
 
 for x in range(1000):
     data = {
-        "value": random.randint(1, 2),
-        "objeto": "patera"
+        "num_object": random.randint(1, 2),
+        "objects": "patera"
     }
     r = requests.post('http://localhost:5000/post/8', json=data)
     print(x)
 
 for x in range(500):
     data = {
-        "value": 0,
-        "objeto": "patera"
+        "num_object": 0,
+        "objects": "patera"
     }
     r = requests.post('http://localhost:5000/post/8', json=data)
     print(x)
 
 for x in range(700):
     data = {
-        "value": 1,
-        "objeto": "patera"
+        "num_object": 1,
+        "objects": "patera"
     }
     r = requests.post('http://localhost:5000/post/8', json=data)
     print(x)
 
 for x in range(300):
     data = {
-        "value": 0,
-        "objeto": "patera"
+        "num_object": 0,
+        "objects": "patera"
     }
     r = requests.post('http://localhost:5000/post/8', json=data)
     print(x)
